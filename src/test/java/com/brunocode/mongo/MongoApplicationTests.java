68753eb29b3faf114746c4134c28056ed01acc84
brunowill-code <brunowilliam.code@gmail.com>
2026-01-19 10:00:57 -0300

    Project created

---
+++ src/test/java/com/brunocode/mongo/MongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunocode.mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
